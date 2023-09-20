f3b5f6b6a3fa9b00e4e5cfbf6f6d0f7141b57aba
OlebogengBogopane <125870308+OlebogengBogopane@users.noreply.github.com>
2023-09-20 13:21:21 +0200

    anchor tags not supporrted with link

--- blog/my-app/components/layout/main-navigation.js
+++ blog/my-app/components/layout/main-navigation.js
@@ -5,24 +5,24 @@ import Logo from './logo'
 
 function MainNavigation() {
   return (
-    <header className={classes.header}>
-        <Link href="/">
-            <a>
-             <Logo />
-            </a>
-        </Link>
-        <nav>
-            <ul>
-            <li>
-                <Link href="/posts">Posts</Link>
-                </li>
-                <li>
-                <Link href="/contact">Contact</Link>
-            </li>
-            </ul>
-        </nav>
-    </header>
-  )
+      <header className={classes.header}>
+          <Link href="/">
+
+              <Logo />
+
+          </Link>
+          <nav>
+              <ul>
+              <li>
+                  <Link href="/posts">Posts</Link>
+                  </li>
+                  <li>
+                  <Link href="/contact">Contact</Link>
+              </li>
+              </ul>
+          </nav>
+      </header>
+  );
 }
 
 export default MainNavigation
